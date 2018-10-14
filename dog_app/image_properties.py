7172738ac946ea4e0eb230e9b0d12d7757efb405
Swiggle45 <jpswingle3@me.com>
2018-10-14 00:39:35 -0400

    stuff

--- dog_app/image_properties.py
+++ dog_app/image_properties.py
@@ -13,12 +13,15 @@ def detect_properties(path):
     response = client.image_properties(image=image)
     props = response.image_properties_annotation
     print('Properties:')
+    total = 0
 
     for color in props.dominant_colors.colors:
+        total += color.pixel_fraction
         print('fraction: {}'.format(color.pixel_fraction))
         print('\tr: {}'.format(color.color.red))
         print('\tg: {}'.format(color.color.green))
         print('\tb: {}'.format(color.color.blue))
         print('\ta: {}'.format(color.color.alpha))
+    print("\nThis is total: {}\n".format(total))
 
 print (detect_properties("Doggo.jpg"))
